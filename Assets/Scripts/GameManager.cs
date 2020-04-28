7eb0cb3d2447a9bd000a51044531a63b103b31b7
irega97 <ivanreq97@gmail.com>
2020-04-28 03:12:23 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //Usamos Singleton
+    public static GameManager instance;
+
+    public BoardManager boardScript;
+
+    private void Awake(){
+        //Iniciamos Singleton
+        if(GameManager.instance==null){
+            GameManager.instance=this;
+        }else if(GameManager.instance != this){
+            Destroy(gameObject);
+        }
+
+        //Que no se destruya el objecto gameManager al cambiar de escenas
+        DontDestroyOnLoad(gameObject);
+
+        boardScript = GetComponent<BoardManager>();
+    }
+
+    private void Start(){
+        InitGame();
+    }
+
+    void InitGame(){
+        boardScript.SetupScene(2);
+    }
+}
